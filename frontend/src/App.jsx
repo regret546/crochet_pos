005698e97ec971c88cdd32882db61f8c692132e0
unknown <kwiniepearl>
2025-10-07 03:23:58 +0800

    initial frontend file structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className=" h-[100vh] w-[100%] bg-pink-300"> </div>
+    </>
+  )
+}
+
+export default App
